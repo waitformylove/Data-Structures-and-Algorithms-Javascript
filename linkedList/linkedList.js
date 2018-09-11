e9d73d89e8af6355fd035e5058e69d8c3398e249
tmbtmbtmb <phamduyhoan95@gmail.com>
2018-09-11 14:04:23 +0700

    Add the add the middle method

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -2,112 +2,152 @@
 
 //add constructors for LinkedList
 function LinkedList(){
-	this.head = null;
-	this.tail = null;
+    this.head = null;
+    this.tail = null;
 }
 //print LinkedList
 LinkedList.prototype.print = function (){
-	console.log('Head = ', this.head);
-	console.log('Tail = ', this.tail);
+    console.log('Head = ', this.head);
+    console.log('Tail = ', this.tail);
 }
 
 //add constructors for a Node in LinkedList
 function Node(value, next, prev){
-	this.value 	= value;
-	this.next  	= next;
-	this.prev   = prev;
+    this.value 	= value;
+    this.next  	= next;
+    this.prev   = prev;
 };
 
 //add a value (in a new Node) to a head of LinkedList (LikedList's prototype)
 LinkedList.prototype.addToHead = function(value){
-	//make a newnode as a head
-	// next of newHeadnode is head before
-	const newHeadNode  = new Node(value, this.head, null);//const is constant and blocked-scope
-	// prev of before head node (if exists) is newnode
-	//if not exist head node => add tail of linkedList
-	//finally, update head of LinkedList
-	if(this.head) 
-		this.head.prev = newHeadNode;
-	else 
-		this.tail      = newHeadNode;
+    //make a newnode as a head
+    // next of newHeadnode is head before
+    const newHeadNode  = new Node(value, this.head, null);//const is constant and blocked-scope
+    // prev of before head node (if exists) is newnode
+    //if not exist head node => add tail of linkedList
+    //finally, update head of LinkedList
+    if(this.head) 
+        this.head.prev = newHeadNode;
+    else 
+        this.tail      = newHeadNode;
 
-	this.head          = newHeadNode;	
+    this.head          = newHeadNode;	
 }
 //add a value (in a new Node) to a tail of LinkedList (LikedList's prototype)
 LinkedList.prototype.addToTail = function (value){
-	//make a newnode as a tail
-	// prev of newHeadnode is tail before
-	const newTailNode  = new Node (value, null , this.tail);
-	//next of before tail node (if exists)   is new node
-	//if not exists tail node => add head of linkedList
-	//finally, update tail of LinkedList
-	if (this.tail)
-		this.tail.next = newTailNode;
-	else
-		this.tail      = newTailNode;
+    //make a newnode as a tail
+    // prev of newHeadnode is tail before
+    const newTailNode  = new Node (value, null , this.tail);
+    //next of before tail node (if exists)   is new node
+    //if not exists tail node => add head of linkedList
+    //finally, update tail of LinkedList
+    if (this.tail)
+        this.tail.next = newTailNode;
+    else
+        this.tail      = newTailNode;
 
-	this.tail          = newTailNode;
+    this.tail          = newTailNode;
+}
+//add a value (in a newNode) to a middle of LinkedList
+LinkedList.prototype.addToMiddle = function (value, position){
+    //catch position tail or head:
+    if (position==0) 
+        return this.addToHead(value);
+    else{//traversing to position -1 
+        let k = 1;
+        let tempNodeP = new Node (null,null,null);
+        let tempNodeQ = new Node (null,null,null);
+        tempNodeP = this.head;
+        tempNodeQ = this.head;
+        while ( (tempNodeP!= null) && ( k < position-1)){
+            k += 1;
+            tempNodeQ = tempNodeP;
+            tempNodeP = tempNodeP.next; 
+        }
+        if(tempNodeP==null){//insert at the end
+            return this.addToTail(value); 
+        }
+        else{//insert at the middle
+            const newMiddleNode  = new Node (value,null , tempNodeP);
+            tempNodeQ.next = newMiddleNode;
+        }
+    }
 }
 
+
+
+
+
+
+
+//////////////////////////////////////
 LinkedList.prototype.removeHead = function() {
-	if(!this.head) 
-		return null; // empty array
-	
-	let value = this.head.value; //save value to return function
-	this.head = this.head.next; 
-	//next node of before head will become now head
-	//check exist of now head and set it now (set prev to null) .
-	//else => linked list now have only 0 elements = > this.head now = null => set this.tail = null
-	if(this.head) 
-		this.head.prev = null;
-	else
-		this.tail      = null;
+    if(!this.head) 
+        return null; // empty array
+
+    let value = this.head.value; //save value to return function
+    this.head = this.head.next; 
+    //next node of before head will become now head
+    //check exist of now head and set it now (set prev to null) .
+    //else => linked list now have only 0 elements = > this.head now = null => set this.tail = null
+    if(this.head) 
+        this.head.prev = null;
+    else
+        this.tail      = null;
 };
 
 LinkedList.prototype.removeTail = function() {
-	if(!this.tail)
-		return null; //empty array
-	
-	let value = this.tail.value;
-	this.tail = this.tail.prev;
+    if(!this.tail)
+        return null; //empty array
 
-	if(this.tail)
-		this.tail.next = null;
-	else
-		this.head      = null;
+    let value = this.tail.value;
+    this.tail = this.tail.prev;
 
-	return value;
+    if(this.tail)
+        this.tail.next = null;
+    else
+        this.head      = null;
+
+    return value;
 };
 
 //search a Node has a value method
 LinkedList.prototype.search = function(value) {
-	let currentNode = this.head;
-	while (currentNode){
-		if (currentNode.value == value) 
-			return currentNode;
-		else
-			currentNode = currentNode.next;
-	}
-	return null;
+    let currentNode = this.head;
+    while (currentNode){
+        if (currentNode.value == value) 
+            return currentNode;
+        else
+            currentNode = currentNode.next;
+    }
+    return null;
 };
+
 
 //start main
 
 var list = new LinkedList();
 list.addToHead(2);
 list.addToHead(1);
-list.addToTail(3);
-list.addToTail(4);
-list.addToTail(5);
+//list.addToTail(3);
+//list.addToTail(4);
+//list.addToTail(5);
 
-list.removeHead();
-list.removeTail();
+//list.removeHead();
+//list.removeTail();
 
-var nodeSearched;
-if( nodeSearched = list.search(5)) 
-	console.log("Find Node : ", nodeSearched);
-else
-	console.log("Not find that value in LIST");
+//var nodeSearched;
+//if( nodeSearched = list.search(5)) 
+//    console.log("Find Node : ", nodeSearched);
+//    else
+//    console.log("Not find that value in LIST");
+//
+//    console.log("Remaining List is");
+//    list.print();  // Let understand circular
+console.log("Before add the middle " );
+list.print();
+list.addToMiddle(3,1);
+console.log("After add the middle " );
+list.print();
 
-console.log("Remaining List is");
-list.print();  // Let understand circular
+
